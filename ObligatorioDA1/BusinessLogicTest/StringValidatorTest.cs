660a8193057fff0fc7a7b8b24e3fc07290507019
Juan Diego Etcheverry Faccelli <JE252443@fi365.ort.edu.uy>
2023-04-02 18:49:22 -0300

    Created StringValidatorTest

---
+++ ObligatorioDA1/BusinessLogicTest/StringValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class StringValidatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
