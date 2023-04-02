3490a37a52e917ad775f86211d1cb032a945865d
Juan Diego Etcheverry Faccelli <JE252443@fi365.ort.edu.uy>
2023-04-02 19:42:08 -0300

    HasSpaces LetterSpace Test

--- ObligatorioDA1/BusinessLogicTest/StringValidatorTest.cs
+++ ObligatorioDA1/BusinessLogicTest/StringValidatorTest.cs
@@ -27,5 +27,12 @@ namespace BusinessLogicTest
             bool hasSpaces = StringValidator.HasSpaces("T");
             Assert.IsFalse(hasSpaces);
         }
+
+        [TestMethod]
+        public void HasSpaces_Letter_Space_True_OK_Test()
+        {
+            bool hasSpaces = StringValidator.HasSpaces("T ");
+            Assert.IsTrue(hasSpaces);
+        }
     }
 }
