6bc55aca07d23a753f871944eb0014c43b82ba26
Nicolas Edelman <nico.edelman@gmail.com>
2023-04-03 15:54:50 -0300

    First test method

---
+++ ObligatorioDA1/BusinessLogic/Shape.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessLogic
+{
+    public class Shape
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ObligatorioDA1/BusinessLogicTest/ShapeTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class ShapeTest
+    {
+        [TestMethod]
+        public void CreateNewShape_Test_OK() {
+            Shape newshape = new Shape();
+            Assert.IsNull(newshape.Name);
+        }
+    }
+}
