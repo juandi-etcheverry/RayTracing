080690d65b809126f3c6810277b512c5fc6cc23c
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-04-03 23:03:48 -0300

    Refactored to extend validation methos on to primitive strings

--- ObligatorioDA1/BusinessLogic/StringValidator.cs
+++ ObligatorioDA1/BusinessLogic/StringValidator.cs
@@ -10,15 +10,33 @@ namespace BusinessLogic
     {
         public static bool HasSpaces(string stringToValidate)
         {
-            return stringToValidate.Contains(" ");
+            return stringToValidate.HasSpaces();
         }
 
         public static bool HasTrailingSpaces(string stringToValidate)
         {
-            return stringToValidate.StartsWith(" ") || stringToValidate.EndsWith(" ");
+            return stringToValidate.HasTrailingSpaces();
         }
 
         public static bool IsEmpty(string stringToValidate)
+        {
+            return stringToValidate.IsEmpty();
+        }
+    }
+
+    public static class StringValidatorExtension
+    {
+        public static bool HasSpaces(this string stringToValidate)
+        {
+            return stringToValidate.Contains(" ");
+        }
+
+        public static bool HasTrailingSpaces(this string stringToValidate)
+        {
+            return stringToValidate.StartsWith(" ") || stringToValidate.EndsWith(" ");
+        }
+
+        public static bool IsEmpty(this string stringToValidate)
         {
             return stringToValidate.Length == 0;
         }
