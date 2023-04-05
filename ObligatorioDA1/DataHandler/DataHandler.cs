8ee5f58cdf11d5676e57161247ed0fe7b500f11f
NicolasEdelman <111385023+NicolasEdelman@users.noreply.github.com>
2023-04-05 18:32:30 -0300

    Feature/da1 3/create shape class (#2)
    * First test method
    
    * CreateNewShape_Name_Test_OK
    
    * Create data handler Test is not null OK
    
    * AddShape - AddOneShapeTest
    
    * Refactor DataHandlerClass
    
    * AddShape - AddAnotherShape Test
    
    * AddShapes - TestCleanup RemoveAllShapes
    
    * AddShape - AddShapeUniqueName Test
    
    * AddShapes - UniqueName Pass
    
    * DataHandler Refactor
    
    * Fixed Dependency Issues
    
    * Generalized IsNameUniqueMethod
    
    * Unsaved changes
    
    * Merge AuxBranch
    
    * AddShape - EmptyName Test Fail
    
    * AddShape - Generic IsNameEmpty
    
    * AddShape - HasTrailingSpaces Fail
    
    * AddShape - Generic Has trailing spaces
    
    * AddShape - AddShape trailing spaces Test OK
    
    * DeleteShape - Delete one shape test
    
    * DeleteShape - DeleteOneSpecificShape Test
    
    * DeleteShape - ShapeNotInList Test Fail
    
    * DeleteShape - ShapeNotInList Test OK
    
    * RenameShape - Rename Existing Shape Test Fail
    
    * RenameShape - Implement RenameShape
    
    * RenameShape - RenameExisting shape Test FAIL

---
+++ ObligatorioDA1/DataHandler/DataHandler.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.XPath;
+
+namespace DataHandlers
+{
+    public static class DataHandler
+    {
+        
+        public static List<Shape> Shapes { get; } = new List<Shape>();
+
+        private static void IsNameUnique<T>(T objectX, List<T> objectList) where T : Shape
+        {
+            bool doesNameExist = objectList.Exists((currentObject) => objectX.AreNamesEqual(currentObject));
+            if (doesNameExist) objectX.ThrowNameExists();
+
+        }
+        private static void IsNameEmpty<T>(T objectX) where T : Shape 
+        {
+            if (StringValidatorExtension.IsEmpty(objectX.Name)) objectX.ThrowEmptyName();
+        }
+
+        private static void HasTrailingSpaces<T>(T objectX) where T : Shape
+        {
+            if (StringValidatorExtension.HasTrailingSpaces(objectX.Name)) objectX.ThrowHasTrailingSpaces();
+        }
+
+        public static void AddShape(Shape oneShape)
+        {
+            HasTrailingSpaces(oneShape);
+            IsNameEmpty(oneShape);
+            IsNameUnique(oneShape, Shapes); 
+            Shapes.Add(oneShape);
+        }
+
+        public static void RemoveAllShapes() { Shapes.Clear(); }
+
+        public static void DeleteShape(Shape shape1)
+        {
+            bool removed = Shapes.Remove(shape1);
+            if (!removed) shape1.ThrowNotInList();
+        }
+        public static void RenameShape(Shape shape, string newName)
+        {
+            Shape aux = new Shape();
+            aux.Name = newName;
+            IsNameUnique<Shape>(aux, Shapes);
+            shape.Name = newName;
+        }
+    }
+}
