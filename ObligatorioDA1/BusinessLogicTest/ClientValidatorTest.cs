5fd8e5bf5f4d483c2e1b4cca4f3edfc10086cd62
Mateo <mateo.goldwasser@hotmail.com>
2023-04-05 18:43:59 -0300

    signup client nameOKpasswordOK test

---
+++ ObligatorioDA1/BusinessLogicTest/ClientValidatorTest.cs
@@ -0,0 +1,27 @@
+﻿using DataHandlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class ClientValidatorTest
+    {
+        [TestCleanup]
+        public void DataHandler_RemoveAllShapes()
+        {
+            DataHandler.RemoveAllClients();
+        }
+
+        [TestMethod]
+        public void SignUp_Client_Name_OK_Password_OK_Test()
+        {
+            Client newClient = new Client();
+            newClient.Name = "Nicolas";
+            newClient.Password = "password123";
+            DataHandler.AddClient(newClient);
+            Assert.AreEqual(1, DataHandler.Clients.Count);
+        }
+    }
+}
