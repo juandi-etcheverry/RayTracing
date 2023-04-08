daf93e3515570dc79e883d53b2a7c892dda9b595
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-04-08 15:01:33 -0300

    Class instancing and test name fixes

---
+++ ObligatorioDA1/BusinessLogicTest/MaterialTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BusinessLogic;
+using DataHandlers;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class MaterialTest
+    {
+        [TestMethod]
+        public void Name_ValidLambertianMaterial_True_OK_Test()
+        {
+            Material m = new Material()
+            {
+                Name = "Pasto",
+                Color = (25, 180, 35),
+                Type = MaterialType.Lambertian
+            };
+            DataHandler.AddMaterial(m);
+            Assert.AreEqual(1, DataHandler.Materials.Count);
+        }
+    }
+}
