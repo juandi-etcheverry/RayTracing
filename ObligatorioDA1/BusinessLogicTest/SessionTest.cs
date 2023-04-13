7cc74598be8263035b987d0d9f35407f826799f9
Mateo <mateo.goldwasser@hotmail.com>
2023-04-13 09:31:19 -0300

    Initialize_Session_Test_OK RED

---
+++ ObligatorioDA1/BusinessLogicTest/SessionTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BusinessLogic;
+using Domain;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class SessionTest
+    {
+        [TestMethod]
+        public void Initialize_Session_Test_OK()
+        {
+            ClientLogic clientLogic = new ClientLogic();
+            Client client = new Client()
+            {
+                Name = "Mateo",
+                Password = "ValidPassword123"
+            };
+            clientLogic.AddClient(client);
+
+            clientLogic.InitializeSession(client);
+
+            Assert.AreEqual(client, clientLogic.GetLoggedClient());
+        }
+    }
+}
