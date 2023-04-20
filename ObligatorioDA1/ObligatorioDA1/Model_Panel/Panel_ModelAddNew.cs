b04eb33d698723059df8ec0fb82a2f62efd503fb
NicolasEdelman <nico.edelman@gmail.com>
2023-04-20 19:22:26 -0300

    Created and designed Model Panels

---
+++ ObligatorioDA1/ObligatorioDA1/Model_Panel/Panel_ModelAddNew.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObligatorioDA1.Model_Panel
+{
+    public partial class Panel_ModelAddNew : UserControl
+    {
+        public Panel_ModelAddNew()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ObligatorioDA1/ObligatorioDA1/Model_Panel/Panel_ModelRename.Designer.cs
+++ ObligatorioDA1/ObligatorioDA1/Model_Panel/Panel_ModelRename.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace ObligatorioDA1.Material_Panel
+﻿namespace ObligatorioDA1.Model_Panel
 {
-    partial class Panel_MaterialRename
+    partial class Panel_ModelRename
     {
         /// <summary> 
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblRenameException = new System.Windows.Forms.Label();
             this.btnConfirmRename = new System.Windows.Forms.Button();
-            this.txbMaterialRename = new System.Windows.Forms.TextBox();
+            this.txbModellRename = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnReturnRename = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
@@ -42,13 +42,13 @@
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
             this.panel1.Controls.Add(this.lblRenameException);
             this.panel1.Controls.Add(this.btnConfirmRename);
-            this.panel1.Controls.Add(this.txbMaterialRename);
+            this.panel1.Controls.Add(this.txbModellRename);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(54, 111);
+            this.panel1.Location = new System.Drawing.Point(55, 111);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(489, 196);
-            this.panel1.TabIndex = 2;
+            this.panel1.TabIndex = 3;
             // 
             // lblRenameException
             // 
@@ -66,23 +66,22 @@
             this.btnConfirmRename.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnConfirmRename.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConfirmRename.ForeColor = System.Drawing.Color.White;
-            this.btnConfirmRename.Location = new System.Drawing.Point(167, 141);
+            this.btnConfirmRename.Location = new System.Drawing.Point(167, 134);
             this.btnConfirmRename.Margin = new System.Windows.Forms.Padding(2);
             this.btnConfirmRename.Name = "btnConfirmRename";
             this.btnConfirmRename.Size = new System.Drawing.Size(168, 32);
             this.btnConfirmRename.TabIndex = 2;
             this.btnConfirmRename.Text = "Rename";
             this.btnConfirmRename.UseVisualStyleBackColor = false;
-            this.btnConfirmRename.Click += new System.EventHandler(this.btnConfirmRename_Click);
             // 
-            // txbMaterialRename
+            // txbModellRename
             // 
-            this.txbMaterialRename.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMaterialRename.Location = new System.Drawing.Point(74, 70);
-            this.txbMaterialRename.Margin = new System.Windows.Forms.Padding(2);
-            this.txbMaterialRename.Name = "txbMaterialRename";
-            this.txbMaterialRename.Size = new System.Drawing.Size(338, 28);
-            this.txbMaterialRename.TabIndex = 1;
+            this.txbModellRename.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbModellRename.Location = new System.Drawing.Point(74, 70);
+            this.txbModellRename.Margin = new System.Windows.Forms.Padding(2);
+            this.txbModellRename.Name = "txbModellRename";
+            this.txbModellRename.Size = new System.Drawing.Size(338, 28);
+            this.txbModellRename.TabIndex = 1;
             // 
             // label1
             // 
@@ -99,16 +98,15 @@
             // 
             this.btnReturnRename.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnReturnRename.ForeColor = System.Drawing.Color.White;
-            this.btnReturnRename.Location = new System.Drawing.Point(54, 58);
+            this.btnReturnRename.Location = new System.Drawing.Point(55, 60);
             this.btnReturnRename.Margin = new System.Windows.Forms.Padding(2);
             this.btnReturnRename.Name = "btnReturnRename";
             this.btnReturnRename.Size = new System.Drawing.Size(82, 37);
-            this.btnReturnRename.TabIndex = 4;
+            this.btnReturnRename.TabIndex = 5;
             this.btnReturnRename.Text = "Return";
             this.btnReturnRename.UseVisualStyleBackColor = false;
-            this.btnReturnRename.Click += new System.EventHandler(this.btnReturnRename_Click);
             // 
-            // Panel_MaterialRename
+            // Panel_ModelRename
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -117,7 +115,7 @@
             this.Controls.Add(this.panel1);
             this.MaximumSize = new System.Drawing.Size(600, 520);
             this.MinimumSize = new System.Drawing.Size(600, 520);
-            this.Name = "Panel_MaterialRename";
+            this.Name = "Panel_ModelRename";
             this.Size = new System.Drawing.Size(600, 520);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -130,7 +128,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblRenameException;
         private System.Windows.Forms.Button btnConfirmRename;
-        private System.Windows.Forms.TextBox txbMaterialRename;
+        private System.Windows.Forms.TextBox txbModellRename;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnReturnRename;
     }
