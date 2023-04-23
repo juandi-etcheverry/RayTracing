53618ef81876f65ccb40d9ebb77e6a0f8e2ad90c
NicolasEdelman <nico.edelman@gmail.com>
2023-04-23 19:54:04 -0300

    created AddNewScene panel

---
+++ ObligatorioDA1/ObligatorioDA1/Panel_SceneAddNew.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObligatorioDA1
+{
+    public partial class Panel_SceneAddNew : UserControl
+    {
+        public Panel_SceneAddNew()
+        {
+            InitializeComponent();
+        }
+    }
+}
