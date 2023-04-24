a50ae639725f81434f0dc752bd083b5f939e5b0f
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-04-23 22:37:01 -0300

    CreateVector RED

---
+++ ObligatorioDA1/GraphicsEngineTest/VectorTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GraphicsEngineTest
+{
+    [TestClass]
+    public class VectorTest
+    {
+        [TestMethod]
+        public void CreateVector_ValidXYZ_OK()
+        {
+            Vector v = new Vector()
+            {
+                x = 0.3,
+                y = 1.0,
+                z = 0.0
+            };
+
+            Assert.AreEqual((0, 3, 1.0, 0.0), (v.x, v.y, v.z));
+        }
+    }
+}
