3401910f1b4fdcce218de2781a0d6282afa6cb1c
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-04-23 22:41:37 -0300

    CreateVector GREEN

--- ObligatorioDA1/GraphicsEngine/Vector.cs
+++ ObligatorioDA1/GraphicsEngine/Vector.cs
@@ -8,5 +8,8 @@ namespace GraphicsEngine
 {
     public class Vector
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
     }
 }
