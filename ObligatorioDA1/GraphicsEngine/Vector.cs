f81fad60e91d01bf75913efb27636aa28a834331
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-03 20:27:01 -0300

    Removed Limits for Vector Coordinates

--- ObligatorioDA1/GraphicsEngine/Vector.cs
+++ ObligatorioDA1/GraphicsEngine/Vector.cs
@@ -4,51 +4,23 @@ namespace GraphicsEngine
 {
     public class Vector
     {
-        private decimal _x;
-        private decimal _y;
-        private decimal _z;
-        public decimal X
-        {
-            get => _x;
-            set
-            {
-                ValidateVectorIsBetweenZeroAndOne(value, "X");
-                _x = value;
-            }
-        }
-        public decimal Y
-        {
-            get => _y;
-            set
-            {
-                ValidateVectorIsBetweenZeroAndOne(value, "Y");
-                _y = value;
-            }
-        }
-        public decimal Z
-        {
-            get => _z;
-            set
-            {
-                ValidateVectorIsBetweenZeroAndOne(value, "Z");
-                _z = value;
-            }
-        }
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Z { get; set; }
 
-        // C# Math.Round method rounds down in .5 values
         public int Red()
         {
-            return Convert.ToInt32(Math.Round(_x * 255, MidpointRounding.AwayFromZero));
+            return Convert.ToInt32(Math.Round(X * 255, MidpointRounding.AwayFromZero));
         }
 
         public int Green()
         {
-            return Convert.ToInt32(Math.Round(_y * 255, MidpointRounding.AwayFromZero));
+            return Convert.ToInt32(Math.Round(Y * 255, MidpointRounding.AwayFromZero));
         }
 
         public int Blue()
         {
-            return Convert.ToInt32(Math.Round(_z * 255, MidpointRounding.AwayFromZero));
+            return Convert.ToInt32(Math.Round(Z * 255, MidpointRounding.AwayFromZero));
         }
 
         private int CalculateCoordinateColor(decimal coordinate)
@@ -70,28 +42,6 @@ namespace GraphicsEngine
         private int ConvertedColorToInt32(decimal coordinate)
         {
             return Convert.ToInt32(coordinate);
-        }
-
-        private void ValidateVectorIsBetweenZeroAndOne(decimal value, string coordinateName)
-        {
-            ValidateVectorIsZeroOrGreater(value, coordinateName);
-            ValidateVectorIsOneOrBelow(value, coordinateName);
-        }
-
-        private void ValidateVectorIsZeroOrGreater(decimal value, string coordinateName)
-        {
-            if (value < 0) ThrowVectorCoordinateIsOutOfRange(coordinateName);
-
-        }
-
-        private void ValidateVectorIsOneOrBelow(decimal value, string coordinateName)
-        {
-            if (value > 1) ThrowVectorCoordinateIsOutOfRange(coordinateName);
-        }
-
-        private void ThrowVectorCoordinateIsOutOfRange(string coordinateName)
-        {
-            throw new ArgumentOutOfRangeException("{0} coordinate must be between 0 and 1", coordinateName);
         }
 
         public Vector Add(Vector vector)
