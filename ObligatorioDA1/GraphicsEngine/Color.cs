a139fa87299107fdb7c5f34e9b8afae75fe30f17
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-04 09:11:42 -0300

    Transfered Red,Green,Blue methods to Color class

---
+++ ObligatorioDA1/GraphicsEngine/Color.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsEngine
+{
+    public class Color : Vector
+    {
+        private decimal _x { get; set; }
+        private decimal _y { get; set; }
+        private decimal _z { get; set; }
+        public decimal X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public decimal Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public decimal Z
+        {
+            get => _z;
+            set
+            {
+                _z = value;
+            }
+        }
+
+        public int Red()
+        {
+            return CalculateCoordinateColor(X);
+        }
+
+        public int Green()
+        {
+            return CalculateCoordinateColor(Y);
+        }
+
+        public int Blue()
+        {
+            return CalculateCoordinateColor(Z);
+        }
+
+        private int CalculateCoordinateColor(decimal coordinate)
+        {
+            decimal roundedCoordinateColorValue = RoundedCoordinateColor(coordinate);
+            return ConvertedColorToInt32(roundedCoordinateColorValue);
+        }
+
+        private decimal RoundedCoordinateColor(decimal coordinate)
+        {
+            return Math.Round(CoordinateColorConversion(coordinate), MidpointRounding.AwayFromZero);
+        }
+
+        private decimal CoordinateColorConversion(decimal coordinate)
+        {
+            return coordinate * 255;
+        }
+
+        private int ConvertedColorToInt32(decimal coordinate)
+        {
+            return Convert.ToInt32(coordinate);
+        }
+    }
+}
