85a1c46aa2ca8b0b5b1bb08214e542abea46b1c1
Juan Diego Etcheverry <111452815+juandi-etcheverry@users.noreply.github.com>
2023-05-04 09:00:49 -0300

    Feature/da1 25/create vector class (#16)
    * GraphicsEngine & GraphicsEngineTest libraries created
    
    * CreateVector RED
    
    * CeateVector RED
    
    * CreateVector RED
    
    * CreateVector GREEN
    
    * CreateVector Invalid X RED
    
    * CreateVector Invalid X Green
    
    * CreateVector Invalid Y RED
    
    * CreateVector Invalid Y GREEN
    
    * CreateVector Invaliz Z RED
    
    * CreateVector Invalid Z GREEN
    
    * CreateVector Refactor
    
    * Removed unused dependencies
    
    * GetColors RedColor RED
    
    * GetColor RedColor GREEN
    
    * Clarified Math.round functionality
    
    * GetColors GreenColor RED
    
    * GetColors GreenColor Green
    
    * GetColors Blue RED
    
    * GetColors BlueColor GREEN
    
    * Corrected GetColor Tests
    
    * GetColor BlueColor GREEN
    
    * GetColors REFACTOR
    
    * Refactored Vector Tests for Decimal numbers
    
    * AddVector RED
    
    * AddVector GREEN
    
    * AddVector Red
    
    * Removed Limits for Vector Coordinates
    
    * Removed Commented Code
    
    * AddVector GREEN
    
    * SubstractVectors RED
    
    * SubstractVector GREEN
    
    * SubstractVector Corrected Test
    
    * MultiplyVector Zero RED
    
    * MultiplyVector RED
    
    * MultiplyVector RED
    
    * MultiplyVector GREEN
    
    * DivideVector RED
    
    * DivideVector GREEN
    
    * VectorLength RED
    
    * VectorLength GREEN
    
    * VectorLength RED
    
    * VectorLenght GREEN
    
    * VectorLength refactor
    
    * AddTo RED
    
    * AddTo GREEN
    
    * SubtractFrom RED
    
    * SubtractFrom GREEN
    
    * ScaleUp RED
    
    * ScaleUp GREEN
    
    * ScaleUpBy extra test
    
    * ScaleDownBy RED
    
    * ScaleDownBy GREEN
    
    * ScaleDownBy RED
    
    * ScaleDownBy RED
    
    * ScaleDownBy GREEN
    
    * Vector Refactor
    
    * UnitVector RED
    
    * UnitVector GREEN
    
    * DotProduct RED
    
    * Dot Product GREEN
    
    * CrossProduct RED
    
    * CrossProduct GREEN

---
+++ ObligatorioDA1/GraphicsEngine/Vector.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace GraphicsEngine
+{
+    public class Vector
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Z { get; set; }
+
+        public int Red()
+        {
+            return CalculateCoordinateColor(X);
+        }
+
+        public int Green()
+        {
+            return CalculateCoordinateColor(Y);
+        }
+
+        public int Blue()
+        {
+            return CalculateCoordinateColor(Z);
+        }
+
+        private int CalculateCoordinateColor(decimal coordinate)
+        {
+            decimal roundedCoordinateColorValue = RoundedCoordinateColor(coordinate);
+            return ConvertedColorToInt32(roundedCoordinateColorValue);
+        }
+
+        private decimal RoundedCoordinateColor(decimal coordinate)
+        {
+            return Math.Round(CoordinateColorConversion(coordinate), MidpointRounding.AwayFromZero);
+        }
+
+        private decimal CoordinateColorConversion(decimal coordinate)
+        {
+            return coordinate * 255;
+        }
+
+        private int ConvertedColorToInt32(decimal coordinate)
+        {
+            return Convert.ToInt32(coordinate);
+        }
+
+        public Vector Add(Vector vector)
+        {
+            return new Vector()
+            {
+                X = X + vector.X,
+                Y = Y + vector.Y,
+                Z = Z + vector.Z
+            };
+        }
+
+        public Vector Subtract(Vector vector)
+        {
+            return new Vector()
+            {
+                X = X - vector.X,
+                Y = Y - vector.Y,
+                Z = Z - vector.Z
+            };
+        }
+
+        public Vector Multiply(decimal scalar)
+        {
+            return new Vector()
+            {
+                X = X * scalar,
+                Y = Y * scalar,
+                Z = Z * scalar
+            };
+        }
+
+        public Vector Divide(decimal scalar)
+        {
+            return new Vector()
+            {
+                X = X / scalar,
+                Y = Y / scalar,
+                Z = Z / scalar
+            };
+        }
+
+        public decimal Length()
+        {
+            double squaredLength = SquaredLength();
+            return Convert.ToDecimal(Math.Sqrt(squaredLength));
+        }
+
+        private double SquaredLength()
+        {
+            return Convert.ToDouble(X * X + Y * Y + Z * Z);
+        }
+
+        public void AddTo(Vector vector)
+        {
+            ReplaceCoordinatesWithVector(Add(vector));
+        }
+
+        public void SubtractFrom(Vector vector)
+        {
+            ReplaceCoordinatesWithVector(Subtract(vector));
+        }
+
+        public void ScaleUpBy(decimal scalar)
+        {
+            ReplaceCoordinatesWithVector(Multiply(scalar));
+        }
+
+        public void ScaleDownBy(decimal scalar)
+        {
+            ReplaceCoordinatesWithVector(Divide(scalar));
+        }
+
+        private void ReplaceCoordinatesWithVector(Vector vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+        }
+
+        public Vector Unit()
+        {
+            return Divide(Length());
+        }
+
+        public decimal Dot(Vector vector)
+        {
+            return (X * vector.X) + (Y * vector.Y) + (Z * vector.Z);
+        }
+
+        public Vector Cross(Vector vector)
+        {
+            return new Vector()
+            {
+                X = Y * vector.Z - (Z * vector.Y),
+                Y = Z * vector.X - (X * vector.Z),
+                Z = X * vector.Y - (Y * vector.X),
+            };
+
+        }
+
+    }
+}
