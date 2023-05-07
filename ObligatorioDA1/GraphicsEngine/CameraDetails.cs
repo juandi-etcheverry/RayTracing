ba45a35dcb8925c5b12351a2f43e32f79eb0f404
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-06 23:18:38 -0300

    Parameters for Camera constructor into new class

---
+++ ObligatorioDA1/GraphicsEngine/CameraDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsEngine
+{
+    internal class CameraDetails
+    {
+        internal Vector LookFrom { get; set; }
+        internal Vector LookAt { get; set; }
+        internal Vector Up { get; set; }
+        internal double FieldOfView { get; set; }
+        internal decimal AspectRatio { get; set; }
+    }
+}
