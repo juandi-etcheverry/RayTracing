f6b782aea0cd378adf4f7ee694b44e023793e079
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-08 20:53:01 -0300

    Image Parser RED

---
+++ ObligatorioDA1/GraphicsEngineTest/ImageParserTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphicsEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphicsEngineTest
+{
+
+    [TestClass]
+    public class ImageParserTest
+    {
+        private Color red;
+        private Color green;
+        private Color blue;
+        private Color black;
+
+        [TestInitialize]
+        public void SetupColors()
+        {
+            red = new Color() { R = 1, G = 0, B = 0 };
+            green = new Color() { R = 0, G = 1, B = 0 };
+            blue = new Color() { R = 0, G = 0, B = 1 };
+            black = new Color() { R = 0, G = 0, B = 0 };
+        }
+        [TestMethod]
+        public void ParseTest_2by2Image_OK()
+        {
+            Color[,] data = { { red, green }, { blue, black } };
+            var parser = new ImageParser()
+            {
+                HorizontalResolution = 2,
+                VerticalResolution = 2,
+            };
+            string result = parser.Parse(data);
+            Assert.AreEqual("P3\n2 2\n255 0 0\n0 255 0\n0 0 255\n0 0 0\n", result);
+        }
+
+    }
+}
