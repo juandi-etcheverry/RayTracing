ed0389d89582fbb4163ce8d72ebb9d486d041ad8
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-08 23:48:06 -0300

    SavePixel InvalidPosition RED

--- ObligatorioDA1/GraphicsEngineTest/PPMImageTest.cs
+++ ObligatorioDA1/GraphicsEngineTest/PPMImageTest.cs
@@ -14,10 +14,7 @@ namespace GraphicsEngineTest
         [TestMethod]
         public void SavePixel_ValidPositions_OK()
         {
-            PPMImage image = new PPMImage()
-            {
-                Width = 3,
-            };
+            PPMImage image = new PPMImage(3);
             Color pixelColor = new Color()
             {
                 R = 0.5m,
@@ -26,6 +23,34 @@ namespace GraphicsEngineTest
             };
             image.SavePixel(1, 0, pixelColor);
             Assert.AreEqual(pixelColor, image.PixelData[0, 0]);
+        }
+
+        [TestMethod]
+        public void SavePixel_OtherValidPosition_OK()
+        {
+            PPMImage image = new PPMImage(3);
+            Color pixelColor = new Color()
+            {
+                R = 0.5m,
+                G = 1,
+                B = 0,
+            };
+            image.SavePixel(0, 0, pixelColor);
+            Assert.AreEqual(pixelColor, image.PixelData[1, 0]);
+        }
+
+        [ExpectedException(typeof(OverflowException))]
+        [TestMethod]
+        public void SavePixel_InvalidPosition_FAIL()
+        {
+            PPMImage image = new PPMImage(3);
+            Color pixelColor = new Color()
+            {
+                R = 0.5m,
+                G = 1,
+                B = 0,
+            };
+            image.SavePixel(-1, 0, pixelColor);
         }
     }
 }
