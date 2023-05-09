151db00cb21d7b3ad192d1c080ff3b2da31caea4
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-09 10:11:32 -0300

    Created HitRecord Class

--- ObligatorioDA1/GraphicsEngine/HitRecord.cs
+++ ObligatorioDA1/GraphicsEngine/HitRecord.cs
@@ -8,6 +8,9 @@ namespace GraphicsEngine
 {
     internal class HitRecord
     {
-
+        internal decimal ScalingFactor { get; set; }
+        internal Vector IntersectionPoint { get; set; }
+        internal Vector Normal { get; set; }
+        internal Color Attenuation { get; set; }
     }
 }
