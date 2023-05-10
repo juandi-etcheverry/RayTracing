f29dbcc1e283c6beba8c72ff7baba88a58431dc3
Juan Diego Etcheverry <je252443@fi365.ort.edu.uy>
2023-05-10 18:00:14 -0300

    Created utility random class for testing

---
+++ ObligatorioDA1/GraphicsEngine/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsEngine
+{
+    internal static class RandomGenerator
+    {
+        public static bool ShowDefaultValue { get; set; }
+        public static double DefaultValue { get; set; }
+        private static Random random = new Random();
+
+        public static double NextDouble()
+        {
+            if (ShowDefaultValue) return DefaultValue;
+            return random.NextDouble();
+        }
+    }
+}
