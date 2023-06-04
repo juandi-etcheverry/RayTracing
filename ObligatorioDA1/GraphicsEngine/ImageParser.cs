0509a30d3e78cc41b805c733bb2a396d07551bd2
Juan Diego Etcheverry <111452815+juandi-etcheverry@users.noreply.github.com>
2023-06-04 15:30:21 -0300

    Feature/da1 48/export formats (#39)
    * Bitmap procesing Back
    
    * Export type
    
    * Fixing export Image
    
    * PPM Exporting & Code Cleanup
    
    ---------
    
    Co-authored-by: NicolasEdelman <nico.edelman@gmail.com>

--- ObligatorioDA1/GraphicsEngine/ImageParser.cs
+++ ObligatorioDA1/GraphicsEngine/ImageParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Security.Permissions;
 
 namespace GraphicsEngine
 {
@@ -53,36 +55,52 @@ namespace GraphicsEngine
             int mili = t.Millisecond;
             return year + month + day + hour + min + sec + mili;
         }
+        public static Bitmap CreateBitmapFromPPMImage(PPMImage ppmImage)
+        {
+            string parsedPPM = ppmImage.parser.Parse();
+            Console.WriteLine("Attempting to parse PPM to Bitmap");
+            return StringPPMToBitmap(parsedPPM);
+        }
 
+        public static Bitmap CreateBitmapFromFilePath(string filePath)
+        {
+            if (filePath.EndsWith("ppm"))
+            {
+                return ConvertPpmToBitmap(filePath);
+            }
+            return new Bitmap(filePath);
+        }
+
+        private static Bitmap StringPPMToBitmap(string parsedPPM)
+        {
+            string[] imageLines = parsedPPM.Split('\n');
+            int WIDHT_HEIGHT_LINE = 1;
+            string[] size = imageLines[WIDHT_HEIGHT_LINE].Split(' ');
+            int width = int.Parse(size[0]);
+            int height = int.Parse(size[1]);
+            Bitmap bitmap = new Bitmap(width, height);
+            int linePosition = 3;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    string[] RGBLine = imageLines[linePosition].Split(' ');
+                    int r = int.Parse(RGBLine[0]);
+                    int g = int.Parse(RGBLine[1]);
+                    int b = int.Parse(RGBLine[2]);
+                    var color = System.Drawing.Color.FromArgb(r, g, b);
+                    bitmap.SetPixel(x, y, color);
+                    linePosition++;
+                }
+            }
+            Console.WriteLine("File Parsed");
+            return bitmap;
+        }
         public static Bitmap ConvertPpmToBitmap(string filePath)
         {
             Console.WriteLine("Attempting read...");
-            using (var streamReader = new StreamReader(filePath))
-            {
-                streamReader.ReadLine();
-
-                string[] size = streamReader.ReadLine()?.Split(' ');
-                int width = int.Parse(size[0]);
-                int height = int.Parse(size[1]);
-
-                streamReader.ReadLine();
-
-                Bitmap bitmap = new Bitmap(width, height);
-                for (int y = 0; y < height; y++)
-                {
-                    for (int x = 0; x < width; x++)
-                    {
-                        string[] RGBLine = streamReader.ReadLine().Split(' ');
-                        int r = int.Parse(RGBLine[0]);
-                        int g = int.Parse(RGBLine[1]);
-                        int b = int.Parse(RGBLine[2]);
-                        var color = System.Drawing.Color.FromArgb(r, g, b);
-                        bitmap.SetPixel(x, y, color);
-                    }
-                }
-                Console.WriteLine("File Parsed");
-                return bitmap;
-            }
+            string parsedPPM = File.ReadAllText(filePath);
+            return StringPPMToBitmap(parsedPPM);
         }
     }
 }
--- ObligatorioDA1/ObligatorioDA1/Scene_Panel/Panel_ExportScene.cs
+++ ObligatorioDA1/ObligatorioDA1/Scene_Panel/Panel_ExportScene.cs
@@ -1,10 +1,14 @@
 ﻿using Domain;
+using GraphicsEngine;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -33,7 +37,46 @@ namespace ObligatorioDA1.Scene_Panel
 
         private void btnExport_Click(object sender, EventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = FileType();
+            saveFileDialog.Title = "Save an Image File";
+            Bitmap bitmap = _scene.Preview;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                Console.WriteLine(filePath);
+                Save(bitmap, filePath);
+            }
             _panelGeneral.GoToSceneEditor(_scene);
+        }
+        private string FileType()
+        {
+            if (rbtnJPG.Checked) { return "JPeg Image|*.jpg"; }
+            if (rbtnPNG.Checked) return "Png Image|*.png";
+            return "PPM Image|*.ppm";
+        }
+        private void Save(Bitmap bitmap, string filePath)
+        {
+            if (rbtnPNG.Checked) SavePNG(bitmap, filePath);
+            else if (rbtnJPG.Checked) SaveJPG(bitmap, filePath);
+            else SavePPM(filePath);
+        }
+
+        private void SavePNG(Bitmap bitmap, string filePath)
+        {
+            bitmap.Save(filePath, ImageFormat.Png);
+        }
+
+        private void SaveJPG(Bitmap bitmap, string filePath)
+        {
+            bitmap.Save(filePath, ImageFormat.Jpeg);
+        }
+
+        private void SavePPM(string filePath)
+        {
+            int sceneDateTime = ImageParser.HashDate(_scene.LastRenderDate);
+            string sceneFileName = $"{_scene.OwnerName}_{sceneDateTime}.ppm";
+            File.Copy(sceneFileName, filePath);
         }
     }
 }
