5463cb015df421280756645d6ce7a04db924dcc3
Mateo <mateo.goldwasser@hotmail.com>
2023-06-12 22:21:05 -0300

    ClientWithMaxRenderTime_OK_Test RED

--- ObligatorioDA1/GraphicsEngineTest/GraphicsEngineTest.cs
+++ ObligatorioDA1/GraphicsEngineTest/GraphicsEngineTest.cs
@@ -647,5 +647,95 @@ namespace GraphicsEngineTest
 
             Assert.IsTrue(_renderLogLogic.GetTotalRenderTimeInMinutes() >= 0);
         }
+
+
+        [TestMethod]
+        public void ClientWithMaxRenderTime_OK_Test()
+        {
+            Shape newShape1 = new Sphere
+            {
+                ShapeName = "NewSphere1",
+                Radius = 1
+            };
+            Shape newShape2 = new Sphere
+            {
+                ShapeName = "NewSphere2",
+                Radius = 10
+            };
+
+            _shapeLogic.AddShape(newShape1);
+            _shapeLogic.AddShape(newShape2);
+
+            var newMaterial = new Material
+            {
+                MaterialName = "NewMaterial",
+            };
+            newMaterial.SetColor(234, 3, 90);
+            _materialLogic.Add(newMaterial);
+
+            var newModel1 = new Model
+            {
+                ModelName = "NewModel1",
+                Shape = _shapeLogic.GetShape(newShape1.ShapeName),
+                Material = _materialLogic.Get(newMaterial.MaterialName)
+            };
+            var newModel2 = new Model
+            {
+                ModelName = "NewModel2",
+                Shape = _shapeLogic.GetShape(newShape2.ShapeName),
+                Material = _materialLogic.Get(newMaterial.MaterialName)
+            };
+            _modelLogic.Add(newModel1);
+            _modelLogic.Add(newModel2);
+
+            var newScene = new Scene
+            {
+                SceneName = "NewScene"
+            };
+            _sceneLogic.Add(newScene);
+
+            var anotherScene = new Scene
+            {
+                SceneName = "AnotherScene"
+            };
+            _sceneLogic.Add(anotherScene);
+            Scene updatedAnotherScene = _sceneLogic.Update(anotherScene);
+
+            Scene updatedScene1 = _sceneLogic.Update(newScene);
+
+            Model newModel1Context = _modelLogic.Get(newModel1.ModelName);
+            Model newModel2Context = _modelLogic.Get(newModel2.ModelName);
+
+            _sceneLogic.AddPositionedModel(newModel1Context, (0, 2, 8), updatedScene1.Id);
+            _sceneLogic.AddPositionedModel(newModel2Context, (10, 10, 3), updatedScene1.Id);
+
+            _sceneLogic.AddPositionedModel(newModel1Context, (9, 15, 1), updatedAnotherScene.Id);
+            _sceneLogic.AddPositionedModel(newModel2Context, (0, 5, 2), updatedAnotherScene.Id);
+
+
+
+            Scene updatedScene2 = _sceneLogic.Update(newScene);
+            Scene updatedAnotherScene2 = _sceneLogic.Update(anotherScene);
+
+            var engine = new GraphicsEngine.GraphicsEngine(updatedScene2)
+            {
+                Width = 15
+            };
+
+            var engine2 = new GraphicsEngine.GraphicsEngine(updatedAnotherScene2)
+            {
+                Width = 15
+            };
+
+            RandomGenerator.ShowDefaultValue = true;
+            RandomGenerator.DefaultValue = 0.5;
+
+            var result1 = engine.Render();
+            var result2 = engine.Render();
+            var result3 = engine2.Render();
+
+            Assert.AreEqual("NewClient3", _renderLogLogic.GetCilientWithMaxRenderTime().Client.Name);
+            Assert.IsTrue(_renderLogLogic.GetCilientWithMaxRenderTime().MaxRenderTime >= 0);
+        }
     }
 }
