a8ff51e55e90dd28f22309635642ee9d0c37d8a1
NicolasEdelman <111385023+NicolasEdelman@users.noreply.github.com>
2023-06-13 22:17:19 -0300

    GUI panel Render logs (#46)
    * GUI panel Render logs
    
    * Delete SceneRepositoryInDB 2.cs
    
    ---------
    
    Co-authored-by: Juan Diego Etcheverry <111452815+juandi-etcheverry@users.noreply.github.com>

--- ObligatorioDA1/ObligatorioDA1/Panel_LogsList.Designer.cs
+++ ObligatorioDA1/ObligatorioDA1/Panel_LogsList.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnReturn = new System.Windows.Forms.Button();
             this.dgvLogsList = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblClientTime = new System.Windows.Forms.Label();
             this.lblMostRenderUser = new System.Windows.Forms.Label();
             this.lblTotalTime = new System.Windows.Forms.Label();
             this.lblAvgTime = new System.Windows.Forms.Label();
@@ -51,10 +51,10 @@
             this.btnReturn.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReturn.ForeColor = System.Drawing.Color.White;
-            this.btnReturn.Location = new System.Drawing.Point(108, 67);
-            this.btnReturn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReturn.Location = new System.Drawing.Point(54, 41);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(183, 71);
+            this.btnReturn.Size = new System.Drawing.Size(92, 37);
             this.btnReturn.TabIndex = 2;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = false;
@@ -73,38 +73,38 @@
             this.dgvLogsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvLogsList.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
             this.dgvLogsList.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.DodgerBlue;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.DodgerBlue;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvLogsList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvLogsList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dgvLogsList.ColumnHeadersHeight = 30;
             this.dgvLogsList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dgvLogsList.EnableHeadersVisualStyles = false;
-            this.dgvLogsList.Location = new System.Drawing.Point(108, 157);
-            this.dgvLogsList.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvLogsList.Location = new System.Drawing.Point(18, 82);
+            this.dgvLogsList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvLogsList.Name = "dgvLogsList";
             this.dgvLogsList.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dgvLogsList.RowHeadersVisible = false;
             this.dgvLogsList.RowHeadersWidth = 82;
             this.dgvLogsList.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Gainsboro;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Gainsboro;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
-            this.dgvLogsList.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvLogsList.RowsDefaultCellStyle = dataGridViewCellStyle6;
             this.dgvLogsList.RowTemplate.Height = 33;
-            this.dgvLogsList.Size = new System.Drawing.Size(1308, 548);
+            this.dgvLogsList.Size = new System.Drawing.Size(709, 285);
             this.dgvLogsList.TabIndex = 3;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lblClientTime);
             this.panel1.Controls.Add(this.lblMostRenderUser);
             this.panel1.Controls.Add(this.lblTotalTime);
             this.panel1.Controls.Add(this.lblAvgTime);
@@ -113,45 +113,46 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(108, 752);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(54, 391);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1308, 182);
+            this.panel1.Size = new System.Drawing.Size(654, 95);
             this.panel1.TabIndex = 4;
             // 
-            // label6
+            // lblClientTime
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label6.Location = new System.Drawing.Point(1127, 113);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(40, 44);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "#";
+            this.lblClientTime.AutoSize = true;
+            this.lblClientTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClientTime.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblClientTime.Location = new System.Drawing.Point(564, 59);
+            this.lblClientTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblClientTime.Name = "lblClientTime";
+            this.lblClientTime.Size = new System.Drawing.Size(20, 24);
+            this.lblClientTime.TabIndex = 9;
+            this.lblClientTime.Text = "#";
             // 
             // lblMostRenderUser
             // 
             this.lblMostRenderUser.AutoSize = true;
-            this.lblMostRenderUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostRenderUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMostRenderUser.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblMostRenderUser.Location = new System.Drawing.Point(1044, 69);
-            this.lblMostRenderUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMostRenderUser.Location = new System.Drawing.Point(522, 36);
+            this.lblMostRenderUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMostRenderUser.Name = "lblMostRenderUser";
-            this.lblMostRenderUser.Size = new System.Drawing.Size(201, 44);
+            this.lblMostRenderUser.Size = new System.Drawing.Size(100, 24);
             this.lblMostRenderUser.TabIndex = 8;
             this.lblMostRenderUser.Text = "UserName";
+            this.lblMostRenderUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblTotalTime
             // 
             this.lblTotalTime.AutoSize = true;
             this.lblTotalTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTotalTime.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblTotalTime.Location = new System.Drawing.Point(658, 100);
-            this.lblTotalTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalTime.Location = new System.Drawing.Point(329, 52);
+            this.lblTotalTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTotalTime.Name = "lblTotalTime";
-            this.lblTotalTime.Size = new System.Drawing.Size(40, 44);
+            this.lblTotalTime.Size = new System.Drawing.Size(20, 24);
             this.lblTotalTime.TabIndex = 7;
             this.lblTotalTime.Text = "#";
             // 
@@ -160,10 +161,10 @@
             this.lblAvgTime.AutoSize = true;
             this.lblAvgTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAvgTime.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblAvgTime.Location = new System.Drawing.Point(135, 113);
-            this.lblAvgTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAvgTime.Location = new System.Drawing.Point(68, 59);
+            this.lblAvgTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAvgTime.Name = "lblAvgTime";
-            this.lblAvgTime.Size = new System.Drawing.Size(40, 44);
+            this.lblAvgTime.Size = new System.Drawing.Size(20, 24);
             this.lblAvgTime.TabIndex = 6;
             this.lblAvgTime.Text = "#";
             // 
@@ -171,10 +172,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(617, 69);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(308, 36);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(127, 31);
+            this.label5.Size = new System.Drawing.Size(67, 17);
             this.label5.TabIndex = 5;
             this.label5.Text = "(Minutes)";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -183,10 +184,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(89, 69);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(44, 36);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(138, 31);
+            this.label4.Size = new System.Drawing.Size(73, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "(Seconds)";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -195,10 +196,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1032, 38);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(516, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(226, 31);
+            this.label3.Size = new System.Drawing.Size(120, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "Most render user:";
             // 
@@ -206,10 +207,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(568, 38);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(284, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(226, 31);
+            this.label2.Size = new System.Drawing.Size(120, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Total render time:";
             // 
@@ -217,26 +218,26 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 38);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 31);
+            this.label1.Size = new System.Drawing.Size(141, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Average render time:";
             // 
             // Panel_LogsList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgvLogsList);
             this.Controls.Add(this.btnReturn);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MaximumSize = new System.Drawing.Size(1500, 1000);
-            this.MinimumSize = new System.Drawing.Size(1500, 1000);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(750, 520);
+            this.MinimumSize = new System.Drawing.Size(750, 520);
             this.Name = "Panel_LogsList";
-            this.Size = new System.Drawing.Size(1500, 1000);
+            this.Size = new System.Drawing.Size(750, 520);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLogsList)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -256,7 +257,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblClientTime;
         private System.Windows.Forms.Label lblMostRenderUser;
     }
 }
